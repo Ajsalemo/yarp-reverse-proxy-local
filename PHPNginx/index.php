910fca97ae7e2d467e8362b6d489a21f5bbf1614
Anthony Salemo <ajssalemo@gmail.com>
2022-02-24 21:26:17 -0500

    containerized the yarp project

--- PHPNginx/index.php
+++ PHPNginx/index.php
@@ -3,7 +3,7 @@
 require_once("./config.php");
 require_once("./todo.php");
 
-$conn = new PDO("mysql:$mysqlHost;dbname=$mysqlDatabase", $mysqlUsername, $mysqlPassword);
+$conn = new PDO("mysql:host=$mysqlHost;dbname=$mysqlDatabase", $mysqlUsername, $mysqlPassword);
 $todo = new Todo();
 
 $todo->connect($conn);
