133e7060ac093ec8d530f314a5182792e71f4c81
Anthony Salemo <ajssalemo@gmail.com>
2022-02-27 21:13:54 -0500

    updated the WeatherForecast class to use reusable functions for cURL endpoint construction and responses, added a structured response

--- PHPApache/index.php
+++ PHPApache/index.php
@@ -7,12 +7,12 @@ $newYorkLat = "40.71455";
 $newYorkLng = "-74.00712";
 
 // Tokyo lat/lng coordinates
-$tokyoLat = "35.68321";
-$tokyoLng = "139.80894";
+$tokyoLat = "35.68949";
+$tokyoLng = "139.69171";
 
 // Paris lat/lng coordinates
-$parisLat = "48.863186";
-$parisLng = "2.339754";
+$parisLat = "48.864716";
+$parisLng = "2.349014";
 
 // Open Weather Map API key
 $openWeatherMapApiKey = getenv("OPEN_WEATHER_MAP_API_KEY");
@@ -20,3 +20,5 @@ $openWeatherMapApiKey = getenv("OPEN_WEATHER_MAP_API_KEY");
 $weatherForecast = new WeatherForecast($openWeatherMapApiKey, $newYorkLat, $newYorkLng, $tokyoLat, $tokyoLng, $parisLat, $parisLng);
 
 $weatherForecast->getCurrentWeatherForNewYork();
+$weatherForecast->getCurrentWeatherForTokyo();
+$weatherForecast->getCurrentWeatherForParis();
--- PHPApache/weather.php
+++ PHPApache/weather.php
@@ -10,6 +10,9 @@ class WeatherForecast
     private $parisLng;
     private $openWeatherMapApiKey;
 
+    // ** Params **
+    // $openWeatherMapApiKey, $newYorkLat, $newYorkLng, $tokyoLat, $tokyoLng, $parisLat
+    // The above params are all passed in through the new instance of this class in index.php
     public function __construct($openWeatherMapApiKey, $newYorkLat, $newYorkLng, $tokyoLat, $tokyoLng, $parisLat, $parisLng)
     {
         $this->newYorkLat = $newYorkLat;
@@ -25,16 +28,55 @@ class WeatherForecast
     // This is so we don't have to keep creating a separate variable per city for the endpoint
     public function constructApiEndpointForCity($lat, $lng)
     {
-        $openWeatherMapEndpoint = "api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lng&appid=$this->openWeatherMapApiKey";
+        $openWeatherMapEndpoint = "api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lng&units=imperial&appid=$this->openWeatherMapApiKey";
         return $openWeatherMapEndpoint;
     }
 
+    // This is the response data from calling the OpenWeatherMap API endpoint
+    // This can be shared across city functions
+    public function constructApiEndpointResponse($curl)
+    {
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $headers = array(
+            "Accept: application/json",
+        );
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        // Execute the GET request to the specified endpoint passed in through getCurrentWeatherFor<cityname>
+        $response = curl_exec($curl);
+        // Close the cURL session
+        curl_close($curl);
+        // Push the response to the specified city array
+        $jsonRes = json_decode($response);
+
+        // Send back a structured response
+        echo PHP_EOL;
+        echo "City: " . $jsonRes->name . ", " . $jsonRes->sys->country . PHP_EOL;
+        echo "Forecast: " . $jsonRes->weather[0]->main . PHP_EOL;
+        echo "Description: " . $jsonRes->weather[0]->description . PHP_EOL;
+        echo "Temperature: " . $jsonRes->main->temp . PHP_EOL;
+        echo "Temperature (feels like): " . $jsonRes->main->feels_like . PHP_EOL;
+        echo "Wind: " . $jsonRes->wind->speed . PHP_EOL;
+        echo PHP_EOL;
+    }
+
+    // Get the current weather for New York
     public function getCurrentWeatherForNewYork()
     {
         $curl = curl_init($this->constructApiEndpointForCity($this->newYorkLat, $this->newYorkLng));
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($curl);
-        curl_close($curl);
-        echo $response . PHP_EOL;
+        $this->constructApiEndpointResponse($curl);
+    }
+
+    // Get the current weather for Tokyo
+    public function getCurrentWeatherForTokyo()
+    {
+        $curl = curl_init($this->constructApiEndpointForCity($this->tokyoLat, $this->tokyoLng));
+        $this->constructApiEndpointResponse($curl);
+    }
+
+    // Get the current weather for Paris
+    public function getCurrentWeatherForParis()
+    {
+        $curl = curl_init($this->constructApiEndpointForCity($this->parisLat, $this->parisLng));
+        $this->constructApiEndpointResponse($curl);
     }
 }
